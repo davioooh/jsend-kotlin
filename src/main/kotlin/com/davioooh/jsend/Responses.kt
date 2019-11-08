3e56bc1570bc169e9fdc96602ad79d167489c885
David Castelletti <d.castelletti@cogitoo.com>
2019-11-08 11:35:29 +0100

    Add responses definition

---
+++ src/main/kotlin/com/davioooh/jsend/Responses.kt
@@ -0,0 +1,52 @@
+package com.davioooh.jsend
+
+import com.davioooh.jsend.Status.*
+
+
+/**
+ * JSend response status.
+ *
+ * @author David Castelletti
+ */
+enum class Status { SUCCESS, FAIL, ERROR }
+
+
+/**
+ * Generic JSend response.
+ *
+ * @param <T> type of response data.
+ *
+ * @author David Castelletti
+ */
+abstract class JSendResponse<T>(val status: Status, val data: T? = null)
+
+
+/**
+ * Success response.
+ *
+ * @param <T> type of response data.
+ *
+ * @author David Castelletti
+ */
+class SuccessResponse<T>(data: T? = null) : JSendResponse<T>(SUCCESS, data)
+
+
+/**
+ * Fail response.
+ *
+ * @param <T> type of response data.
+ *
+ * @author David Castelletti
+</T> */
+class FailResponse<T>(data: T? = null) : JSendResponse<T>(FAIL, data)
+
+
+/**
+ * Error response.
+ *
+ * @param <T> type of response data.
+ *
+ * @author David Castelletti
+</T> */
+class ErrorResponse<T>(val message: String, val code: Int? = null, data: T? = null) :
+    JSendResponse<T>(ERROR, data)
