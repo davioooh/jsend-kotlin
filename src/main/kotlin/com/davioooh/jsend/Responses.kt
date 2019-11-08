084a3a05cdcab80a412af38acbf15cce30944fe8
David Castelletti <d.castelletti@cogitoo.com>
2019-11-08 12:10:54 +0100

    Fix KDoc

--- src/main/kotlin/com/davioooh/jsend/Responses.kt
+++ src/main/kotlin/com/davioooh/jsend/Responses.kt
@@ -14,7 +14,9 @@ enum class Status { SUCCESS, FAIL, ERROR }
 /**
  * Generic JSend response.
  *
- * @param <T> type of response data.
+ * @param T type of response data.
+ * @property status response status.
+ * @property data response data (optional for some types of response).
  *
  * @author David Castelletti
  */
@@ -24,7 +26,8 @@ abstract class JSendResponse<T>(val status: Status, val data: T? = null)
 /**
  * Success response.
  *
- * @param <T> type of response data.
+ * @param T type of response data.
+ * @property data response data (optional).
  *
  * @author David Castelletti
  */
@@ -34,19 +37,23 @@ class SuccessResponse<T>(data: T? = null) : JSendResponse<T>(SUCCESS, data)
 /**
  * Fail response.
  *
- * @param <T> type of response data.
+ * @param T type of response data.
+ * @property data response data.
  *
  * @author David Castelletti
-</T> */
+ */
 class FailResponse<T>(data: T) : JSendResponse<T>(FAIL, data)
 
 
 /**
  * Error response.
  *
- * @param <T> type of response data.
+ * @param T type of response data.
+ * @property message error message.
+ * @property code error code (optional).
+ * @property data response data (optional).
  *
  * @author David Castelletti
-</T> */
+ */
 class ErrorResponse<T>(val message: String, val code: Int? = null, data: T? = null) :
     JSendResponse<T>(ERROR, data)
